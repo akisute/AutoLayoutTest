d0fbbefedd3134f8f7a7cc34a2ebcd4f9d2a709d
akisute <akisutesama@gmail.com>
2014-11-23 21:48:21 +0900

    test 3

--- AutoLayoutTest/Test3ViewController.swift
+++ AutoLayoutTest/Test3ViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class Test3ViewController: UIViewController {
 
+    @IBOutlet var blue: UIView!
+    @IBOutlet var blueBottom: NSLayoutConstraint!
+    
+    @IBAction func onButton(sender: UIButton) {
+        self.blue.hidden = true
+        UIView.animateWithDuration(0.3, animations: {
+            self.blueBottom.priority = 1
+            self.blueBottom.active = false // iOS 8 or later only
+            self.view.setNeedsUpdateConstraints()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    @IBAction func onReset(sender: UIBarButtonItem) {
+        self.blue.hidden = false
+        UIView.animateWithDuration(0.3, animations: {
+            self.blueBottom.priority = 999 // UILayoutPriorityRequired - 1
+            self.blueBottom.active = true // iOS 8 or later only
+            self.view.setNeedsUpdateConstraints()
+            self.view.layoutIfNeeded()
+        })
+    }
 }
